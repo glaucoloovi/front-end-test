aaa1affb52ecd72fd4d209c73cb5fbade0714778
Vinicius Gabriel <viiniciusgs03@gmail.com>
2024-06-24 21:06:00 -0300

    chore: add montserrat font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,19 @@
 import StyledComponentsRegistry from '@/lib/registry'
 import { GlobalStyle } from '@/styles/global'
+import { Montserrat } from 'next/font/google'
 
 type RootLayoutProps = {
   children: React.ReactNode
 }
 
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className={montserrat.className}>
       <body>
         <StyledComponentsRegistry>
           <GlobalStyle />
