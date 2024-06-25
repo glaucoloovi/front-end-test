4497b588e367c7dd227d053499a7542e4a54388f
Vinicius Gabriel <viiniciusgs03@gmail.com>
2024-06-24 21:00:51 -0300

    fix: remove prettier format on lintstaged to avoid conflicts with eslint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,9 +2,8 @@ module.exports = {
   // this will check Typescript files
   '**/*.(ts|tsx)': () => 'npx tsc --noEmit --skipLibCheck --incremental',
 
-  // This will lint and format TypeScript files
+  // This will lint TypeScript files
   '**/*.(ts|tsx)': (filenames) => [
     `npx eslint --fix ${filenames.join(' ')}`,
-    `npx prettier --write ${filenames.join(' ')}`,
   ],
 };
